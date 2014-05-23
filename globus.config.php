934eaacee6fcd69df6a0741d5ef3796c8e2c761c
Alex Gor <alex.wpstarter@gmail.com>
2014-05-23 19:50:22 +0800

    Initial commit

---
+++ globus.config.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ *
+ */
+
+class WPGlobus_Config {
+
+	/*
+	 * Plugin name
+	 */
+	const GLOBUS_PLUGIN_NAME = 'globus';
+
+	/*
+	 *	Url mode: query (questionmark)
+	 */
+	const GLOBUS_URL_QUERY = 1;
+
+	/*
+	 *	Url mode: pre-path
+	 */
+	const GLOBUS_URL_PATH = 2;
+
+	/*
+	 *	Url mode: pre-domain
+	 */
+	const GLOBUS_URL_DOMAIN = 3;
+
+	/*
+	 * Current language
+	 *
+	 * @var string
+	 */
+	public $language = 'en';
+
+	/*
+	 * Language by default
+	 *
+	 * @var string
+	 */
+	var $default_language = 'en';
+
+	/*
+	 * Enabled languages
+	 *
+	 * @var array
+	 */
+	var $enabled_languages = array('en','ru','de');
+
+	/*
+	 * Hide from URL language by default
+	 *
+	 * @var bool
+	 */
+	var $hide_default_language = true;
+
+	/*
+	 * URL mode
+	 * query || pre-path || pre-domain
+	 *
+	 * @var int
+	 */
+	var $url_mode;
+
+	/*
+	 *	URL information
+	 *
+	 * @var array
+	 */
+	var $url_info = array();
+
+	/*
+	 *	Flag images configuration
+	 *	Look in /flags/ directory for a huge list of flags for usage
+	 *
+	 * @var array
+	 */
+	var $flag = array();
+
+	/*
+	 * Location of flags (needs trailing slash!)
+	 * 'plugins/globus/flags/';
+	 * @var string
+	 */
+	var $flags_url = '';
+
+	/*
+	 *
+	 * @var array
+	 */
+	var $language_name = array();
+
+	/*
+	 * Use flag name for navigation menu : 'name' || 'code' || ''
+	 * @var string
+	 */
+	var $show_flag_name = 'code';
+
+	/*
+	 * Use navigation menu by slug
+	 * for use in all nav menu set value to ''
+	 * @var string
+	 */
+	var $nav_menu = '';
+
+	/*
+	 * Constructor
+	 */
+	function __construct() {
+		# set default url mode
+		$this->url_mode = self::GLOBUS_URL_PATH;
+		//$this->url_mode = self::GLOBUS_URL_QUERY;
+
+		#
+		$this->_set_flags();
+
+		#
+		$this->_set_language_name();
+
+		#
+		$this->_get_options();
+	}
+
+	/*
+	 *
+	 */
+	function get_url_mode(){
+		return $this->url_mode;
+	}
+
+	/*
+	 *
+	 */
+	function _set_flags(){
+
+		$this->flag['en'] = 'gb.png';
+		$this->flag['ru'] = 'ru.png';
+		$this->flag['de'] = 'de.png';
+		$this->flag['zh'] = 'cn.png';
+		$this->flag['fi'] = 'fi.png';
+		$this->flag['fr'] = 'fr.png';
+		$this->flag['nl'] = 'nl.png';
+		$this->flag['sv'] = 'se.png';
+		$this->flag['it'] = 'it.png';
+		$this->flag['ro'] = 'ro.png';
+		$this->flag['hu'] = 'hu.png';
+		$this->flag['ja'] = 'jp.png';
+		$this->flag['es'] = 'es.png';
+		$this->flag['vi'] = 'vn.png';
+		$this->flag['ar'] = 'arle.png';
+		$this->flag['pt'] = 'br.png';
+		$this->flag['pt'] = 'br.png';
+		$this->flag['gl'] = 'galego.png';
+
+		$this->_set_flags_url();
+	}
+
+	/*
+	 *
+	 */
+	function _set_flags_url() {
+		$this->flags_url = plugins_url(self::GLOBUS_PLUGIN_NAME . '/flags/');
+	}
+
+	/*
+	 *
+	 */
+	function _set_language_name() {
+		// Names for languages in the corresponding language, add more if needed
+		$this->language_name['en'] = "English";
+		$this->language_name['ru'] = "Russian";
+		$this->language_name['de'] = "Deutsch";
+		$this->language_name['zh'] = "中文";
+		$this->language_name['fi'] = "suomi";
+		$this->language_name['fr'] = "Français";
+		$this->language_name['nl'] = "Nederlands";
+		$this->language_name['sv'] = "Svenska";
+		$this->language_name['it'] = "Italiano";
+		$this->language_name['ro'] = "Română";
+		$this->language_name['hu'] = "Magyar";
+		$this->language_name['ja'] = "日本語";
+		$this->language_name['es'] = "Español";
+		$this->language_name['vi'] = "Tiếng Việt";
+		$this->language_name['ar'] = "العربية";
+		$this->language_name['pt'] = "Português";
+		$this->language_name['pl'] = "Polski";
+		$this->language_name['gl'] = "galego";
+	}
+
+	/*
+	 * Get options from DB and wp-config.php
+	 * @return void
+	 */
+	function _get_options(){
+
+		$wpglobus_option = get_option('wpglobus_option');
+		//error_log( print_r( $wpglobus_option, true ) );
+
+		/*
+		 * get default language
+		 */
+		if ( isset( $wpglobus_option['default_language'] ) ) {
+			$this->default_language = $wpglobus_option['default_language'];
+		}
+		if ( defined('WPGLOBUS_DEFAULT_LANGUAGE') ) {
+			$this->default_language = $wpglobus_option['default_language'];
+		}
+
+		/*
+		 * get enabled language
+		 */
+		if ( isset( $wpglobus_option['enabled_languages'] ) && ! empty($wpglobus_option['enabled_languages'])  ) {
+
+			$this->enabled_languages = array();
+			$this->enabled_languages[] = $this->default_language;
+			foreach( $wpglobus_option['enabled_languages'] as $language=>$language_name ) {
+				//error_log( $language . '=>' . $language_name );
+				// error_log();
+				if ( ! empty( $language_name ) ) {
+					$this->enabled_languages[] = $language;
+				}
+			}
+			//$this->enabled_languages[] = 'ru';
+		}
+
+		//error_log( print_r( $this->enabled_languages, true) );
+
+		/*
+		 * get option 'show_flag_name'
+		 */
+		if ( isset( $wpglobus_option['show_flag_name'] ) ) {
+			$this->show_flag_name = $wpglobus_option['show_flag_name'];
+		}
+		if ( defined('WPGLOBUS_SHOW_FLAG_NAME') ) {
+			if ( 'name' === WPGLOBUS_SHOW_FLAG_NAME ) {
+				$this->show_flag_name = 'name';
+			} elseif ( false === WPGLOBUS_SHOW_FLAG_NAME || '' === WPGLOBUS_SHOW_FLAG_NAME ) {
+				$this->show_flag_name = '';
+			}
+		}
+
+		/*
+		 * get navigation menu slug for add flag in front-end 'use_nav_menu'
+		 */
+		if ( isset($wpglobus_option['use_nav_menu']) ) {
+			$this->nav_menu = ( $wpglobus_option['use_nav_menu'] == 'all' ) ? '' : $wpglobus_option['use_nav_menu'];
+		}
+		if ( defined('WPGLOBUS_USE_NAV_MENU') ) {
+			$this->nav_menu = WPGLOBUS_USE_NAV_MENU;
+		}
+	}
+
+}	// end class WPGlobus_Config
---
+++ globus.php
@@ -0,0 +1,287 @@
+<?php
+/*
+Plugin Name: Globus
+Description: Globus translation plugin
+Text Domain: wpglobus
+Version: 0.1
+Author: Alex Gor
+Author URI: 
+*/
+
+// Exit if accessed directly
+if ( !defined('ABSPATH') ) exit;
+
+include( dirname(__FILE__) . '/globus.config.php' );
+global $WPGlobus_Config;
+$WPGlobus_Config = new WPGlobus_Config();
+
+include( dirname(__FILE__) . '/functions.php' );
+
+# extract url information
+$WPGlobus_Config->url_info = globus_extractURL( $_SERVER['REQUEST_URI'], $_SERVER["HTTP_HOST"], isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '' );
+$WPGlobus_Config->language = $WPGlobus_Config->url_info['language'];
+
+
+class WPGlobus {
+
+	public static $_version = '0.1';
+
+	/*
+	 * List navigation menus
+	 * @var array
+	 */
+	var $menus = array();
+
+	private $option = 'wpglobus_option';
+
+	/*
+	 * Constructor
+	 */
+	function __construct() {
+
+		if ( is_admin() ) {
+
+			require_once 'Redux-Framework/ReduxCore/framework.php';
+			require_once 'includes/options/globus-option.php';
+
+			// add_filter( "redux/{$this->option}/field/class/radio_sorter", array( &$this, 'on_radio_sorter' ) );
+			// add_filter( "redux/{$this->option}/field/class/select_with_flag", array( &$this, 'on_select_with_flag' ) );
+			add_filter( "redux/{$this->option}/field/class/select", array( &$this, 'on_select' ) );
+
+			global $WPGlobusOption;
+			$WPGlobusOption = new Redux_Framework_globus_option();
+
+			$wpglobus_option = get_option( $this->option );
+
+			//error_log( print_r( $wpglobus_option, true) );
+
+		} else {
+
+			$this->menus = $this->_get_nav_menus();
+
+			add_filter( 'wp_list_pages', 		array( &$this, 'on_wp_list_pages' ), 10, 99 );
+			add_filter( 'wp_nav_menu_objects', 	array( &$this, 'on_add_item' ), 10, 99 );
+			add_action( 'wp_head', 				array( &$this, 'on_wp_head'  ) );
+			add_action( 'wp_print_styles', 		array( &$this, 'on_wp_styles' ) );
+		}
+
+	}
+
+	function on_select_with_flag($field){
+		return dirname(__FILE__) . '/includes/options/fields/select_with_flag/field_select_with_flag.php';
+	}
+	function on_select($field){
+		return dirname(__FILE__) . '/includes/options/fields/select/field_select.php';
+	}
+	/*
+	 * Enqueue styles
+	 * @return void
+	 */
+	function on_wp_styles(){
+		wp_register_style(
+			'flags',
+			plugins_url( '/globus.flags.css', __FILE__ ),
+			array(),
+			'',
+			'all'
+		);
+		wp_enqueue_style( 'flags' );
+	}
+
+	/*
+	 * Add css styles to head section
+	 * @return string
+	 */
+	function on_wp_head() {
+
+		global $WPGlobus_Config;
+
+		$css  = "<style type=\"text/css\" media=\"screen\">\n";
+		foreach( $WPGlobus_Config->enabled_languages as $language) {
+			$css .= ".globus-flag-" . $language . " { background:url(" . $WPGlobus_Config->flags_url . $WPGlobus_Config->flag[$language] . ") no-repeat }\n";
+		}
+		$css  .="</style>\n";
+
+		echo $css;
+
+	}
+
+
+	/*
+	 * Add item to navigation menu which was created with wp_list_pages
+	 * @return string
+	 */
+	function on_wp_list_pages( $output, $r ) {
+
+		global $WPGlobus_Config;
+
+		$extra_languages = array();
+		foreach( $WPGlobus_Config->enabled_languages as $languages ) {
+			if ( $languages != $WPGlobus_Config->language ) {
+				$extra_languages[] = $languages;
+			}
+		}
+
+		$span_classes 	= array( 'globus-flag', 'globus-language-name' );
+
+		$span_classes_lang   = $span_classes;
+		$span_classes_lang[] = 'globus-flag-' . $WPGlobus_Config->language;
+
+		$output .= '<li class="page_item page-item-globus-menu-switch page_item_has_children">
+						<a href="' . globus_getUrl($WPGlobus_Config->language) . '"><span class="' . implode( ' ', $span_classes_lang ) . '">' . $this->_get_flag_name( $WPGlobus_Config->language ) . '</span></a>
+						<ul class="children">';
+		foreach( $extra_languages as $language ) {
+			$span_classes_lang	 = $span_classes;
+			$span_classes_lang[] = 'globus-flag-' . $language;
+			$output .= 		'<li id="" class="page_item">
+								<a href="' . globus_getUrl($language) . '"><span class="' . implode( ' ', $span_classes_lang ) .'">' . $this->_get_flag_name( $language ) . '</span></a>
+							</li>';
+		}	// end foreach
+		$output .= 	'	</ul>
+					</li>';
+
+		return $output;
+	}
+
+	/*
+	 * Add item to navigation menu
+	 * @return array
+	 */
+	function on_add_item( $sorted_menu_items, $args ) {
+
+		global $WPGlobus_Config;
+
+		if ( ! empty( $WPGlobus_Config->nav_menu ) ) {
+			if ( ! isset($args->menu->slug) ) {
+				if ( $this->menus[0]->slug != $WPGlobus_Config->nav_menu ) {
+					return $sorted_menu_items;
+				}
+			} elseif ( $args->menu->slug != $WPGlobus_Config->nav_menu ) {
+				return $sorted_menu_items;
+			}
+		}
+
+		$extra_languages = array();
+		foreach( $WPGlobus_Config->enabled_languages as $languages ) {
+			if ( $languages != $WPGlobus_Config->language ) {
+				$extra_languages[] = $languages;
+			}
+		}
+
+		$item_classes = array( '', 'menu-item', 'menu-item-type-post_type', 'menu-item-globus-menu-switch' );
+
+		$span_classes 	= array( 'globus-flag', 'globus-language-name' );
+
+		$span_classes_lang	 = $span_classes;
+		$span_classes_lang[] = 'globus-flag-' . $WPGlobus_Config->language;
+
+		$item = new stdClass();
+		$item->ID				= 9999999999; // 9 999 999 999
+		$item->db_id			= 9999999999;
+		$item->menu_item_parent = 0;
+		$item->title 			= '<span class="' . implode( ' ', $span_classes_lang ) . '">' . $this->_get_flag_name( $WPGlobus_Config->language ) . '</span>';
+		$item->url 				= globus_getUrl( $WPGlobus_Config->language );
+		$item->classes  		= $item_classes;
+
+		$sorted_menu_items[]  	= $item;
+
+		foreach( $extra_languages as $language ) {
+			$span_classes_lang 		= $span_classes;
+			$span_classes_lang[]	= 'globus-flag-' . $language;
+
+			$item = new stdClass();
+			$item->ID				= 'globus-menu-switch-' . $language;
+			$item->db_id			= 'globus-menu-switch-' . $language;
+			$item->menu_item_parent = 9999999999;
+			$item->title 			= '<span class="' . implode( ' ', $span_classes_lang ) . '">' . $this->_get_flag_name( $language ) . '</span>';
+			$item->url 				= globus_getUrl( $language );
+			$item->classes  		= $item_classes;
+
+			$sorted_menu_items[]  	= $item;
+		}
+
+		return $sorted_menu_items;
+	}
+
+	/*
+	 * Get flag name for navigation menu
+	 * @return string
+	 */
+	function _get_flag_name( $language ) {
+
+		global $WPGlobus_Config;
+
+		switch ( $WPGlobus_Config->show_flag_name ) {
+			case 'name' :
+				$flag_name = $WPGlobus_Config->language_name[$language] ;
+				break;
+			case 'code' :
+				$flag_name = $language;
+				break;
+			default:
+				$flag_name = '';
+		}
+		return $flag_name;
+
+	}
+
+	/*
+	 * Get navigation menus
+	 * @return array
+	 */
+	public static function _get_nav_menus() {
+		/** @global wpdb $wpdb */
+		global $wpdb;
+
+		$query = "SELECT * FROM {$wpdb->prefix}terms AS t
+					  LEFT JOIN {$wpdb->prefix}term_taxonomy AS tt ON tt.term_id = t.term_id
+					  WHERE tt.taxonomy = 'nav_menu'";
+
+		$menus = $wpdb->get_results( $query );
+
+		return $menus;
+
+	}
+
+}
+
+$WPGlobus = new WPGlobus();
+
+
+
+// globus_extractURL();
+
+//error_log( print_r( $menus, true ) );
+
+
+//add_filter( 'wp_nav_menu_items', 'on_add_item', 10, 2);  // 4 items from menu
+//add_filter( 'wp_nav_menu_args', 'on_add_item1', 10 );
+function on_add_item1( $args ) {
+	error_log( print_r( $args, true ) );
+	return $args;
+}
+
+//add_filter( 'wp_nav_menu', 'on_add_item', 10, 2);  // 6 items ( without switcher )
+function on_add_item( $items, $args ) {
+	global $globus_config;
+	
+	//error_log( print_r( $_SERVER, true ) );
+	
+	$url1 = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/';
+	
+	$item = '<ul class="globus-top-switch-submenu sub-menu">'; 	// position: relative;	
+		
+	foreach( $globus_config['enabled_languages'] as $language ) {
+		if ( $globus_config['language'] != $language ) {
+			$item .= '<li class="menu-item"><a href="' . $url1 . $language . $_SERVER['REQUEST_URI'] . '">' . $language . '</a></li>';
+		}
+	}
+	
+	$item .= '</ul>';
+
+	$items = str_replace('<span class="globus-switch-extend"></span></a>', '</a>' . $item, $items );
+	//error_log( print_r( $items, true ) );
+	//error_log( print_r( $args, true ) );
+	
+	return $items;
+}
