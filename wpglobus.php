c70a9f4d436e30e2fdc8d515fdbe32950da50a06
Gregory K <gregory@tiv.net>
2014-06-19 11:53:36 -0400

    Updated plugin header

--- wpglobus.php
+++ wpglobus.php
@@ -1,11 +1,30 @@
 <?php
 /*
-Plugin Name: WPGlobus
-Description: WPGlobus translation plugin
-Text Domain: wpglobus
-Version: 0.2
-Author: Alex Gor
-Author URI: 
+ * Plugin Name: WPGlobus
+ * Plugin URI: https://github.com/WPGlobus/WPGlobus
+ * Description: WordPress internationalization helper
+ * Text Domain: wpglobus
+ * Domain Path: /languages/
+ * Version: 0.2.0
+ * Author: WPGlobus
+ * Author URI: http://www.wpglobus.com/
+ * Network: false
+ * License: GPL2
+ *
+	Copyright 2014 Alex Gor (alexgff) and Gregory Karpinsky (tivnet)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 // Exit if accessed directly
