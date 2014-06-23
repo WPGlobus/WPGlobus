25842044207ff96c0cc38a479f73f1fb4802579c
Alex Gor <alex.wpstarter@gmail.com>
2014-06-23 10:28:13 +0800

    Cleanup js

--- includes/options/fields/table/field_table.js
+++ includes/options/fields/table/field_table.js
@@ -1,73 +1,24 @@
 /* global redux_change */
-(function($) {
+(function ($) {
     "use strict";
 
     $.redux = $.redux || {};
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $.redux.table();
     });
 
     /**
      * Table
-     * Dependencies		: jquery
+     * Dependencies: jquery
      */
-    $.redux.table = function() {
+    $.redux.table = function () {
         var $t_flag = $('.flag-table-wrapper').html(),
             $t_form_table = $('.flag-table-wrapper').parents('table');
 
-        $t_form_table.wrap( '<div style="overflow:hidden;" class="flag-table"></div>' );
+        $t_form_table.wrap('<div style="overflow:hidden;" class="flag-table"></div>');
         $t_form_table.remove();
-        $('.flag-table').html( $t_flag );
+        $('.flag-table').html($t_flag);
 
-
-        $('#add_language').on('click', function() {
-            //console.log('clicked');
-            $('.table-dummy').toggleClass('hidden');
-
-        });
-        return;
-
-        $(".cb-enable").click(function() {
-            if ($(this).hasClass('selected')) {
-                return;
-            }
-
-            var parent = $(this).parents('.switch-options');
-
-            $('.cb-disable', parent).removeClass('selected');
-            $(this).addClass('selected');
-            $('.checkbox-input', parent).val(1);
-
-            redux_change($('.checkbox-input', parent));
-
-            //fold/unfold related options
-            var obj     = $(this);
-            var $fold   = '.f_' + obj.data('id');
-
-            $($fold).slideDown('normal', "swing");
-        });
-        $(".cb-disable").click(function() {
-
-            if ($(this).hasClass('selected')) {
-                return;
-            }
-
-            var parent = $(this).parents('.switch-options');
-
-            $('.cb-enable', parent).removeClass('selected');
-            $(this).addClass('selected');
-            $('.checkbox-input', parent).val(0);
-
-            redux_change($('.checkbox-input', parent));
-
-            //fold/unfold related options
-            var obj     = $(this);
-            var $fold   = '.f_' + obj.data('id');
-
-            $($fold).slideUp('normal', "swing");
-        });
-
-        $('.cb-enable span, .cb-disable span').find().attr('unselectable', 'on');
     };
 })(jQuery);
