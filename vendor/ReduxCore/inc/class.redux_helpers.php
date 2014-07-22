c6d3f869c6da6ff225ea9685e1e60be0736e8c33
Alex Gor <alex.wpstarter@gmail.com>
2014-07-22 12:28:05 +0800

    Updated Redux core to 3.3.4.9

--- vendor/ReduxCore/inc/class.redux_helpers.php
+++ vendor/ReduxCore/inc/class.redux_helpers.php
@@ -1,47 +1,29 @@
 <?php
 
 // Exit if accessed directly
-if( !defined( 'ABSPATH' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
 // Don't duplicate me!
-if( !class_exists( 'Redux_Helpers' ) ) {
+if ( ! class_exists( 'Redux_Helpers' ) ) {
 
     /**
      * Redux Helpers Class
-     *
      * Class of useful functions that can/should be shared among all Redux files.
      *
      * @since       1.0.0
      */
     class Redux_Helpers {
 
-        public static function curlRead($filename){
-            $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL, $filename);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            
-            $data = curl_exec($ch);
-            
-            curl_close($ch);
-            
-            if (empty($data)) {
-                $data = false;
-            }
-            
-            return $data;
-        }
-        
-        public static function tabFromField($parent, $field) {
-          foreach( $parent->sections as $k => $section ) {
-                if ( !isset( $section['title'] ) ){
+        public static function tabFromField( $parent, $field ) {
+            foreach ( $parent->sections as $k => $section ) {
+                if ( ! isset( $section['title'] ) ) {
                     continue;
                 }
 
-                if ( isset( $section['fields'] ) && !empty( $section['fields'] ) ) {
-                    if ( Redux_Helpers::recursive_array_search($field, $section['fields'])) {
+                if ( isset( $section['fields'] ) && ! empty( $section['fields'] ) ) {
+                    if ( Redux_Helpers::recursive_array_search( $field, $section['fields'] ) ) {
                         return $k;
                         continue;
                     }
@@ -49,14 +31,24 @@ if( !class_exists( 'Redux_Helpers' ) ) {
             }
         }
 
-        public static function isFieldInUse($parent, $field) {
-          foreach( $parent->sections as $k => $section ) {
-                if ( !isset( $section['title'] ) ){
+        public static function isFieldInUseByType( $fields, $field = array() ) {
+            foreach ( $field as $name ) {
+                if ( array_key_exists( $name, $fields ) ) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static function isFieldInUse( $parent, $field ) {
+            foreach ( $parent->sections as $k => $section ) {
+                if ( ! isset( $section['title'] ) ) {
                     continue;
                 }
 
-                if ( isset( $section['fields'] ) && !empty( $section['fields'] ) ) {
-                    if ( Redux_Helpers::recursive_array_search($field, $section['fields'])) {
+                if ( isset( $section['fields'] ) && ! empty( $section['fields'] ) ) {
+                    if ( Redux_Helpers::recursive_array_search( $field, $section['fields'] ) ) {
                         return true;
                         continue;
                     }
@@ -64,17 +56,31 @@ if( !class_exists( 'Redux_Helpers' ) ) {
             }
         }
 
-        public static function isParentTheme($file) {
-            if (strpos(self::cleanFilePath($file), self::cleanFilePath(get_template_directory())) !== false) {
+        public static function isParentTheme( $file ) {
+            $file   = self::cleanFilePath( $file );
+            $dir    = self::cleanFilePath( get_template_directory() );
+            
+            $file   = str_replace('//', '/', $file);
+            $dir    = str_replace('//', '/', $dir);
+            
+            if ( strpos( $file, $dir ) !== false ) {
                 return true;
             }
+
             return false;
         }
 
-        public static function isChildTheme($file) {
-            if (strpos(self::cleanFilePath($file), self::cleanFilePath(get_stylesheet_directory())) !== false) {
+        public static function isChildTheme( $file ) {
+            $file   = self::cleanFilePath( $file );
+            $dir    = self::cleanFilePath( get_stylesheet_directory() );
+            
+            $file   = str_replace('//', '/', $file);
+            $dir    = str_replace('//', '/', $dir);
+            
+            if ( strpos( $file, $dir ) !== false ) {
                 return true;
             }
+
             return false;
         }
 
@@ -82,74 +88,106 @@ if( !class_exists( 'Redux_Helpers' ) ) {
             return ReduxFramework::$_as_plugin;
         }
 
-        public static function isTheme ($file) {
-            if (true == self::isChildTheme($file) || true == self::isParentTheme($file)) {
+        public static function isTheme( $file ) {
+            if ( true == self::isChildTheme( $file ) || true == self::isParentTheme( $file ) ) {
                 return true;
             }
+
             return false;
         }
 
-        public static function array_in_array($needle, $haystack) {
+        public static function array_in_array( $needle, $haystack ) {
             //Make sure $needle is an array for foreach
-            if (!is_array($needle)) {
-                $needle = array($needle);
+            if ( ! is_array( $needle ) ) {
+                $needle = array( $needle );
             }
             //For each value in $needle, return TRUE if in $haystack
-            foreach ($needle as $pin)
-            //echo 'needle' . $pin;
-                if (in_array($pin, $haystack)) {
+            foreach ( $needle as $pin ) //echo 'needle' . $pin;
+            {
+                if ( in_array( $pin, $haystack ) ) {
                     return true;
                 }
+            }
+
             //Return FALSE if none of the values from $needle are found in $haystack
             return false;
         }
 
-        public static function recursive_array_search($needle, $haystack) {
-            foreach($haystack as $key => $value) {
-                if($needle === $value || (is_array($value) && self::recursive_array_search($needle, $value) !== false)) {
+        public static function recursive_array_search( $needle, $haystack ) {
+            foreach ( $haystack as $key => $value ) {
+                if ( $needle === $value || ( is_array( $value ) && self::recursive_array_search( $needle, $value ) !== false ) ) {
                     return true;
                 }
             }
+
             return false;
         }
 
         /**
          * Take a path and return it clean
+         *
          * @param string $path
+         *
          * @since    3.1.7
          */
         public static function cleanFilePath( $path ) {
-            $path = str_replace('','', str_replace( array( "\\", "\\\\" ), '/', $path ) );
-            if ($path[ strlen($path)-1 ] === '/') {
-                $path = rtrim($path, '/');
+            $path = str_replace( '', '', str_replace( array( "\\", "\\\\" ), '/', $path ) );
+            
+            if ( $path[ strlen( $path ) - 1 ] === '/' ) {
+                $path = rtrim( $path, '/' );
             }
+
             return $path;
         }
 
         /**
-         * Field Render Function.
+         * Take a path and delete it
          *
+         * @param string $path
+         *
+         * @since    3.3.3
+         */
+        public static function rmdir( $dir ) {
+            if ( is_dir( $dir ) ) {
+                $objects = scandir( $dir );
+                foreach ( $objects as $object ) {
+                    if ( $object != "." && $object != ".." ) {
+                        if ( filetype( $dir . "/" . $object ) == "dir" ) {
+                            rrmdir( $dir . "/" . $object );
+                        } else {
+                            unlink( $dir . "/" . $object );
+                        }
+                    }
+                }
+                reset( $objects );
+                rmdir( $dir );
+            }
+        }
+
+        /**
+         * Field Render Function.
          * Takes the color hex value and converts to a rgba.
          *
          * @since ReduxFramework 3.0.4
          */
-        public static function hex2rgba($hex, $alpha = '') {
-            $hex = str_replace("#", "", $hex);
-            if(strlen($hex) == 3) {
-                $r = hexdec(substr($hex,0,1).substr($hex,0,1));
-                $g = hexdec(substr($hex,1,1).substr($hex,1,1));
-                $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+        public static function hex2rgba( $hex, $alpha = '' ) {
+            $hex = str_replace( "#", "", $hex );
+            if ( strlen( $hex ) == 3 ) {
+                $r = hexdec( substr( $hex, 0, 1 ) . substr( $hex, 0, 1 ) );
+                $g = hexdec( substr( $hex, 1, 1 ) . substr( $hex, 1, 1 ) );
+                $b = hexdec( substr( $hex, 2, 1 ) . substr( $hex, 2, 1 ) );
             } else {
-                $r = hexdec(substr($hex,0,2));
-                $g = hexdec(substr($hex,2,2));
-                $b = hexdec(substr($hex,4,2));
+                $r = hexdec( substr( $hex, 0, 2 ) );
+                $g = hexdec( substr( $hex, 2, 2 ) );
+                $b = hexdec( substr( $hex, 4, 2 ) );
             }
-            $rgb = $r.','.$g.','.$b;
+            $rgb = $r . ',' . $g . ',' . $b;
 
-            if ('' == $alpha) {
+            if ( '' == $alpha ) {
                 return $rgb;
             } else {
-                $alpha = floatval($alpha);
+                $alpha = floatval( $alpha );
+
                 return 'rgba(' . $rgb . ',' . $alpha . ')';
             }
         }
