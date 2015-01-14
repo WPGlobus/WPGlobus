55c1a8923253dc1615d541801fc1d5f9352def46
Alex Gor <alex.wpstarter@gmail.com>
2015-01-14 14:03:55 +0700

    Hide room for slug

--- includes/class-wpglobus.php
+++ includes/class-wpglobus.php
@@ -1166,6 +1166,7 @@ class WPGlobus {
 						<label class="screen-reader-text" id="title-prompt-text-<?php echo $language; ?>" for="title_<?php echo $language; ?>"><?php echo apply_filters( 'enter_title_here', __( 'Enter title here' ), $post ); ?></label>
 						<input type="text" name="post_title_<?php echo $language; ?>" size="30" value="<?php echo esc_attr( htmlspecialchars( __wpg_text_filter($post->post_title, $language, WPGlobus::RETURN_EMPTY) ) ); ?>" id="title_<?php echo $language;?>" class="title_wpglobus" autocomplete="off" />
 					</div> <!-- #titlewrap -->
+					<!--
 					<div class="inside">
 						<div id="edit-slug-box-<?php echo $language; ?>" class="hide-if-no-js">
 							<b>Slug will be here</b>
