2dc4650480ff1933d2a5201d03cc8246d4b7a408
Alex Gor <alex.wpstarter@gmail.com>
2015-01-18 11:08:13 +0700

    Made vendor.js running with strict mode

--- includes/js/wpglobus.vendor.js
+++ includes/js/wpglobus.vendor.js
@@ -1,13 +1,9 @@
 /*jslint browser: true*/
 /*global jQuery, console, WPGlobusVendor, wpseoMetaboxL10n, yst_updateSnippet */
-jQuery(document).ready(function () {
-    "use strict";
-
-    wpglobus_wpseo = function () {
-        if (typeof wpseoMetaboxL10n === "undefined") {
-            return;
-        }
-        yst_updateSnippet();
-    };
-
-});
+"use strict";
+var wpglobus_wpseo = function () {
+	if (typeof wpseoMetaboxL10n === "undefined") {
+		return;
+	}
+	yst_updateSnippet();
+};
