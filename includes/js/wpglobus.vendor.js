c4f0fbf9fb53819b0671b4257136840af87bf891
Alex Gor <alex.wpstarter@gmail.com>
2015-01-21 12:08:01 +0700

    don't use strict mode like original js wpseo

--- includes/js/wpglobus.vendor.js
+++ includes/js/wpglobus.vendor.js
@@ -1,6 +1,6 @@
 /*jslint browser: true*/
 /*global jQuery, console, WPGlobusVendor, wpseoMetaboxL10n, yst_updateSnippet */
-"use strict";
+// "use strict";
 var wpglobus_wpseo = function () {
 	if (typeof wpseoMetaboxL10n === "undefined") {
 		return;
