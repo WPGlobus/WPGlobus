503a279da2bd20afb08eb285a5175ed99905d924
Alex Gor <alex.wpstarter@gmail.com>
2015-01-21 12:00:53 +0700

    Support Focus Keywords autocomplete and 'change' event

--- includes/js/wpglobus.vendor.js
+++ includes/js/wpglobus.vendor.js
@@ -56,7 +56,6 @@ var wpglobus_wpseo = function () {
 		var esc_sep = yst_escapeFocusKw(wpseoMetaboxL10n.sep);
 		var pattern = new RegExp(esc_sep + ' ' + esc_sep, 'g');
 		str = str.replace(pattern, wpseoMetaboxL10n.sep);
-
 		if (str.indexOf('%%') != -1 && str.match(/%%[a-z0-9_-]+%%/i) != null) {
 			regex = /%%[a-z0-9_-]+%%/gi;
 			matches = str.match(regex);
@@ -317,11 +316,52 @@ var wpglobus_wpseo = function () {
 		jQuery('#wpseosnippet_'+l+' .url').text($e.data('url-'+l));
 		wpglobus_updateSnippet(l);
 		
+		//
+		var cache = {}, lastXhr;
+		jQuery('#' + wpseoMetaboxL10n.field_prefix + 'focuskw' + '_' + l).autocomplete({
+			minLength   : 3,
+			formatResult: function (row) {
+				return jQuery('<div/>').html(row).html();
+			},
+			source      : function (request, response) {
+				var term = request.term;
+				if (term in cache) {
+					response(cache[term]);
+					return;
+				}
+				request._ajax_nonce = wpseoMetaboxL10n.wpseo_keyword_suggest_nonce;
+				request.action = 'wpseo_get_suggest';
+
+				lastXhr = jQuery.getJSON(ajaxurl, request, function (data, status, xhr) {
+					cache[term] = data;
+					if (xhr === lastXhr) {
+						response(data);
+					}
+					return;
+				});
+			}
+		});
+		
 		jQuery('#' + wpseoMetaboxL10n.field_prefix + 'title' + '_' + l).keyup(function () {
 			wpglobus_updateTitle(false, jQuery(this).data('language'));
 		});
 		jQuery('#' + wpseoMetaboxL10n.field_prefix + 'metadesc' + '_' + l).keyup(function () {
 			wpglobus_updateDesc(jQuery(this).data('language'));
+		});
+		
+		var focuskwhelptriggered = false;
+		jQuery(document).on('change', '#' + wpseoMetaboxL10n.field_prefix + 'focuskw' + '_' + l, function () {
+			var l = jQuery(this).data('language');
+			var focuskwhelpElm = jQuery('#focuskwhelp'+'_'+l);
+			if (jQuery('#' + wpseoMetaboxL10n.field_prefix + 'focuskw' + '_' + l).val().search(',') != -1) {
+				focuskwhelpElm.click();
+				focuskwhelptriggered = true;
+			} else if (focuskwhelptriggered) {
+				focuskwhelpElm.qtip("hide");
+				focuskwhelptriggered = false;
+			}
+
+			wpglobus_updateSnippet();
 		});		
 		
 	});
