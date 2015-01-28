b00050a506b1866e7de602efa98e3ff4d5ce882e
Gregory K <gregory@tiv.net>
2015-01-28 15:42:30 -0500

    Function to check caller (not tested properly yet)

--- includes/class-wpglobus-wp.php
+++ includes/class-wpglobus-wp.php
@@ -44,6 +44,52 @@ class WPGlobus_WP {
 		return ( ! empty( $_POST['action'] ) && in_array( $_POST['action'], $action ) );
 	}
 
+	/**
+	 * Check if a filter is called by a certain function / class
+	 *
+	 * @param string $function
+	 * @param string $class
+	 *
+	 * @return bool
+	 * @todo Unit test
+	 * @todo What if we check class only?
+	 * @todo Use the form class::method ?
+	 */
+	public static function is_filter_called_by( $function, $class = '' ) {
+		if ( empty( $function ) ) {
+			return false;
+		}
+
+		/**
+		 * WP calls filters at level 4. This function adds one more level.
+		 */
+		$trace_level = 5;
+
+		$callers = debug_backtrace();
+		if ( empty( $callers[ $trace_level ] ) ) {
+			return false;
+		}
+
+		/**
+		 * First check: if function name matches
+		 */
+		$maybe = ( $callers[ $trace_level ]['function'] === $function );
+
+		if ( $maybe ) {
+			/**
+			 * Now check if we also asked for a specific class, and it matches
+			 */
+			if ( ! empty( $class ) &&
+			     ! empty( $callers[ $trace_level ]['class'] ) &&
+			     $callers[ $trace_level ]['class'] !== $class
+			) {
+				$maybe = false;
+			}
+		}
+
+		return $maybe;
+	}
+
 
 } // class
 
