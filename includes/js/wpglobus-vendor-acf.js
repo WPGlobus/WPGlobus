d680cc24237d02dbee9ef7923c8b530de97d56fd
Alex Gor <alex.wpstarter@gmail.com>
2015-02-24 18:48:48 +0700

    use various style for postbox-container

--- includes/js/wpglobus-vendor-acf.js
+++ includes/js/wpglobus-vendor-acf.js
@@ -10,6 +10,10 @@
 
 jQuery(document).ready(function($){
 	var id;
+	var style = 'width:90%;';
+	if  ( $('.acf_postbox').parents('#postbox-container-2').length == 1 ) {
+		style = 'width:97%';	
+	}	
 	$('.acf_postbox .field').each(function(i,e){
 		var $t = $(this);
 		if ( $t.hasClass('field_type-textarea') ) {
@@ -26,7 +30,7 @@ jQuery(document).ready(function($){
 			$(clone).attr('name', 'wpglobus-'+name);
 			$(clone).attr('data-source-id', id);
 			$(clone).attr('class', 'wpglobus-dialog-field textarea');
-			$(clone).attr('style', 'width:97%;');
+			$(clone).attr('style', style);
 			$(clone).val( WPGlobusCore.TextFilter($(element).val(), WPGlobusCoreData.language) );
 			$(clone).insertAfter(element);
 			$('<div style="width:20px;float:right;"><input data-type="control" data-source-type="textarea" data-source-id="'+id+'" class="wpglobus_dialog_start" onclick="javascript:void(0);" type="button" style="cursor:pointer;width:20px;" value="..."/></div>').insertAfter(clone);
@@ -43,7 +47,7 @@ jQuery(document).ready(function($){
 			$(clone).attr('name', 'wpglobus-'+name);
 			$(clone).attr('data-source-id', id);
 			$(clone).attr('class', 'wpglobus-dialog-field text');
-			$(clone).attr('style', 'width:97%;');
+			$(clone).attr('style', style);
 			$(clone).val( WPGlobusCore.TextFilter($(element).val(), WPGlobusCoreData.language) );
 			$(clone).insertAfter(element);
 			$('<div style="width:20px;float:right;"><input data-type="control" data-source-type="text" data-source-id="'+id+'" class="wpglobus_dialog_start" onclick="javascript:void(0);" type="button" style="cursor:pointer;width:20px;" value="..."/></div>').insertAfter(clone);
