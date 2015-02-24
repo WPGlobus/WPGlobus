795db19f4039f0c575cdcb913bb36fafc7ede92e
Alex Gor <alex.wpstarter@gmail.com>
2015-02-24 17:22:57 +0700

    Added support field type text ACF

--- includes/js/wpglobus-vendor-acf.js
+++ includes/js/wpglobus-vendor-acf.js
@@ -12,25 +12,43 @@ jQuery(document).ready(function($){
 	var id;
 	$('.acf_postbox .field').each(function(i,e){
 		var $t = $(this);
-		if ( ! $t.hasClass('field_type-textarea') ) {
-			return true;
-		}
-		var element = $t.find('textarea'),
-			clone, name;
-		
-		id = element.attr('id');
-		
-		clone = $('#'+id).clone();
-		$(element).addClass('hidden');
-		name = element.attr('name');
-		$(clone).attr('id', 'wpglobus-'+id);
-		$(clone).attr('name', 'wpglobus-'+name);
-		$(clone).attr('data-source-id', id);
-		$(clone).attr('class', 'wpglobus-dialog-field textarea');
-		$(clone).attr('style', 'width:97%;');
-		$(clone).val( WPGlobusCore.TextFilter($(element).val(), WPGlobusCoreData.language) );
-		$(clone).insertAfter(element);
-		$('<div style="width:20px;float:right;"><input data-type="control" data-source-type="textarea" data-source-id="'+id+'" class="wpglobus_dialog_start" onclick="javascript:void(0);" type="button" style="cursor:pointer;width:20px;" value="..."/></div>').insertAfter(clone);
+		if ( $t.hasClass('field_type-textarea') ) {
 			
+			var element = $t.find('textarea'),
+				clone, name;
+			
+			id = element.attr('id');
+			
+			clone = $('#'+id).clone();
+			$(element).addClass('hidden');
+			name = element.attr('name');
+			$(clone).attr('id', 'wpglobus-'+id);
+			$(clone).attr('name', 'wpglobus-'+name);
+			$(clone).attr('data-source-id', id);
+			$(clone).attr('class', 'wpglobus-dialog-field textarea');
+			$(clone).attr('style', 'width:97%;');
+			$(clone).val( WPGlobusCore.TextFilter($(element).val(), WPGlobusCoreData.language) );
+			$(clone).insertAfter(element);
+			$('<div style="width:20px;float:right;"><input data-type="control" data-source-type="textarea" data-source-id="'+id+'" class="wpglobus_dialog_start" onclick="javascript:void(0);" type="button" style="cursor:pointer;width:20px;" value="..."/></div>').insertAfter(clone);
+		} else if ( $t.hasClass('field_type-text') ) {
+			var element = $t.find('input'),
+				clone, name;
+			
+			id = element.attr('id');
+			
+			clone = $('#'+id).clone();
+			$(element).addClass('hidden');
+			name = element.attr('name');
+			$(clone).attr('id', 'wpglobus-'+id);
+			$(clone).attr('name', 'wpglobus-'+name);
+			$(clone).attr('data-source-id', id);
+			$(clone).attr('class', 'wpglobus-dialog-field text');
+			$(clone).attr('style', 'width:97%;');
+			$(clone).val( WPGlobusCore.TextFilter($(element).val(), WPGlobusCoreData.language) );
+			$(clone).insertAfter(element);
+			$('<div style="width:20px;float:right;"><input data-type="control" data-source-type="text" data-source-id="'+id+'" class="wpglobus_dialog_start" onclick="javascript:void(0);" type="button" style="cursor:pointer;width:20px;" value="..."/></div>').insertAfter(clone);
+
+
+		}	
 	});
 });
