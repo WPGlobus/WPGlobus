4e2b623149ae92b50d39837f429c42edf87247a6
Alex Gor <alex.wpstarter@gmail.com>
2015-02-26 20:08:47 +0700

    Revising js

--- includes/js/wpglobus-widgets.js
+++ includes/js/wpglobus-widgets.js
@@ -13,30 +13,99 @@ window.WPGlobusWidgets;
 	var api;
 	api = WPGlobusWidgets = {
 		init: function() {
-			api.add_elements();	
+			api.add_elements();
+			api.attachListeners();
 		},	
-		add_elements : function(post_id) {
-			var id;
-			$('.widget-liquid-right .widget .widget-content').each(function(i,e){
-				var $t = $(this),
-					element = $t.find('input[type="text"]'),
-					clone, name;
-					
+		add_elements : function(get_by, coid, action) {
+			var id, elem = [];
+			elem[0] = 'input[type="text"]';
+			elem[1] = 'textarea';
+			if ( typeof get_by == 'undefined' || get_by == 'class' ) {
+				get_by_coid = '.widget-liquid-right .widget .widget-content';
+				$.each(elem, function(i,e){
+					api.make_clone(get_by_coid, e);
+				});
+			} else if ( get_by == 'id' ) {
+				get_by_coid = '#'+coid+' .widget-content';
+				$.each(elem, function(i,e){
+					api.make_clone(get_by_coid, e);
+				});	
+			}
+		},
+		make_clone: function(get_by_coid, type) {
+			$(get_by_coid+' '+type).each(function(i,e){
+				var element = $(e),
+					clone, name, text;
+
 				id = element.attr('id');
-				
+				if ( typeof id == 'undefined' || -1 != id.indexOf( '-number') ) {
+					return true;
+				}	
+
 				clone = $('#'+id).clone();
-				//$(element).addClass('wpglobus-dialog-field-source hidden');
-				$(element).addClass('wpglobus-dialog-field-source');
+				$(element).addClass('wpglobus-dialog-field-source hidden');
 				name = element.attr('name');
 				$(clone).attr('id', 'wpglobus-'+id);
 				$(clone).attr('name', 'wpglobus-'+name);
 				$(clone).attr('data-source-id', id);
 				$(clone).attr('class', 'wpglobus-dialog-field');
 				$(clone).attr('style', 'width:90%;');
-				$(clone).val( WPGlobusCore.TextFilter($(element).val(), WPGlobusCoreData.language) );
-				$('<div style="width:20px;" data-type="control" data-source-type="textarea" data-source-id="'+id+'" class="wpglobus-widgets wpglobus_dialog_start wpglobus_dialog_icon"></div>').insertAfter(element);
+				text = WPGlobusCore.TextFilter($(element).val(), WPGlobusCoreData.language);
+				$(clone).val(text);
+				$('<div style="width:20px;" data-type="control" data-source-type="" data-source-id="'+id+'" class="wpglobus-widgets wpglobus_dialog_start wpglobus_dialog_icon"></div>').insertAfter(element);
 				$(clone).insertAfter(element);
+				if ( 'input[type="text"]' == type && '' != text ) {
+					var w_id = element.parents('.widget').attr('id');
+					$('#'+w_id+' .in-widget-title').text(': '+text);
+				}
 			});				
-		}				
+		},	
+		attachListeners: function() {
+			$(document).ajaxComplete(function(ev, jqxhr, settings){
+				if ( -1 != settings.data.indexOf( 'action=save-widget') ) {
+					var response = $.trim(jqxhr.responseText);
+					if ( -1 != settings.data.indexOf( 'delete_widget=1' ) ) {
+						// deleted widget
+					} else {
+						// update or added new widget
+						var s = settings.data.split('widget-id=');
+						s = s[1].split('&');
+						$('.widget-liquid-right .widget').each(function(i,e){
+							var id = $(e).attr('id');
+							if ( -1 !== id.indexOf(s[0]) ) {
+								api.add_elements('id', id);
+							}	
+						});	
+					}	
+				}	
+			});
+			$('body').on('change', '.wpglobus-dialog-field', function(){
+				var $t = $(this),
+					source_id = '#'+$t.data('source-id'),
+					source = '', s = '', new_value;
+					
+				if ( typeof source_id == 'undefined' ) {
+					return;	
+				}	
+				source = $(source_id).val();
+				
+				if ( ! /(\{:|\[:|<!--:)[a-z]{2}/.test(source) ) {
+					$(source_id).val($t.val());
+				} else {
+					$.each(WPGlobusCoreData.enabled_languages, function(i,l){
+						if ( l == WPGlobusCoreData.language ) {
+							new_value = $t.val();
+						} else {	
+							new_value = WPGlobusCore.TextFilter(source,l,'RETURN_EMPTY');
+						}	
+						if ( '' != new_value ) {
+							s = s + WPGlobusCore.addLocaleMarks(new_value,l);	
+						}	
+					});
+					$(source_id).val(s);
+				}	
+
+			});					
+		}	
 	};
 })(jQuery);
