a56edeab910a2c678d343336f791a77c5c23bc95
Alex Gor <alex.wpstarter@gmail.com>
2016-04-01 10:02:52 +0700

    version bump

--- wpglobus.php
+++ wpglobus.php
@@ -6,7 +6,7 @@
  * Description: A WordPress Globalization / Multilingual Plugin. Posts, pages, menus, widgets and even custom fields - in multiple languages!
  * Text Domain: wpglobus
  * Domain Path: /languages/
- * Version: 1.4.10-alpha
+ * Version: 1.5.0-beta
  * Author: WPGlobus
  * Author URI: http://www.wpglobus.com/
  * Network: false
@@ -30,7 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WPGLOBUS_VERSION', '1.4.10-alpha' );
+define( 'WPGLOBUS_VERSION', '1.5.0-beta' );
 define( 'WPGLOBUS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
 /** @todo Get rid of these */
