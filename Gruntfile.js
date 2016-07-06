b8b92761862c494503840fabd5c02b86bcb2088a
Gregory K <gregory@tiv.net>
2016-07-06 18:25:04 -0400

    [Grunt] "composer update" task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 /*jslint node: true */
+/*global require*/
 'use strict';
 
 module.exports = function (grunt) {
@@ -12,8 +13,7 @@ module.exports = function (grunt) {
         pathJS,
         potFile,
         pathCSS_options_fields,
-        pathJS_options_fields
-        ;
+        pathJS_options_fields;
 
     pkgJson = require('./package.json');
     pkgName = pkgJson.name;
@@ -46,7 +46,7 @@ module.exports = function (grunt) {
             options: {
                 screenshot_url: 'https://ps.w.org/wpglobus/assets/{screenshot}.png'
             },
-            main: {
+            main   : {
                 files: {
                     'README.md': 'readme.txt'
                 }
@@ -57,23 +57,23 @@ module.exports = function (grunt) {
          * @link https://github.com/gruntjs/grunt-contrib-uglify
          */
         uglify: {
-            main: {
+            main          : {
                 files: [{
                     expand: true,
-                    cwd: pathJS + '/',
-                    src: ['*.js', '!*.min.js'],
-                    dest: pathJS + '/',
-                    ext: '.min.js'
+                    cwd   : pathJS + '/',
+                    src   : ['*.js', '!*.min.js'],
+                    dest  : pathJS + '/',
+                    ext   : '.min.js'
                 }]
             },
             options_fields: {
                 files: [{
-                    src: [
+                    src    : [
                         pathJS_options_fields + '/**/*.js',
                         '!' + pathJS_options_fields + '/**/*.min.js'
                     ],
-                    ext: '.min.js',
-                    expand: true,
+                    ext    : '.min.js',
+                    expand : true,
                     flatten: false
                 }]
             }
@@ -83,47 +83,47 @@ module.exports = function (grunt) {
          * @link https://github.com/gruntjs/grunt-contrib-less
          */
         less: {
-            main: {
+            main          : {
                 options: {
-                    paths: [pathCSS],
-                    sourceMap: true,
+                    paths            : [pathCSS],
+                    sourceMap        : true,
                     sourceMapBasepath: pathCSS,
-                    sourceMapURL: 'wpglobus.css.map'
+                    sourceMapURL     : 'wpglobus.css.map'
                 },
-                files: {
+                files  : {
                     "includes/css/wpglobus.css": pathCSS + '/' + "wpglobus.less"
                 }
             },
-            admin: {
+            admin         : {
                 options: {
-                    paths: [pathCSS],
-                    sourceMap: true,
+                    paths            : [pathCSS],
+                    sourceMap        : true,
                     sourceMapBasepath: pathCSS,
-                    sourceMapURL: 'wpglobus-admin.css.map'
+                    sourceMapURL     : 'wpglobus-admin.css.map'
                 },
-                files: {
+                files  : {
                     "includes/css/wpglobus-admin.css": pathCSS + '/' + "wpglobus-admin.less"
                 }
             },
-            tabs: {
+            tabs          : {
                 options: {
-                    paths: [pathCSS],
-                    sourceMap: true,
+                    paths            : [pathCSS],
+                    sourceMap        : true,
                     sourceMapBasepath: pathCSS,
-                    sourceMapURL: 'wpglobus-admin-tabs.css.map'
+                    sourceMapURL     : 'wpglobus-admin-tabs.css.map'
                 },
-                files: {
+                files  : {
                     "includes/css/wpglobus-admin-tabs.css": pathCSS + '/' + "wpglobus-admin-tabs.less"
                 }
             },
-            dialogui: {
+            dialogui      : {
                 options: {
-                    paths: [pathCSS],
-                    sourceMap: true,
+                    paths            : [pathCSS],
+                    sourceMap        : true,
                     sourceMapBasepath: pathCSS,
-                    sourceMapURL: 'wpglobus-dialog-ui.css.map'
+                    sourceMapURL     : 'wpglobus-dialog-ui.css.map'
                 },
-                files: {
+                files  : {
                     "includes/css/wpglobus-dialog-ui.css": pathCSS + '/' + "wpglobus-dialog-ui.less"
                 }
             },
@@ -131,10 +131,10 @@ module.exports = function (grunt) {
                 options: {
                     sourceMap: false // Does not work properly with globs
                 },
-                files: [{
-                    src: [pathCSS_options_fields + '/**/*.less'],
-                    ext: '.css',
-                    expand: true,
+                files  : [{
+                    src    : [pathCSS_options_fields + '/**/*.less'],
+                    ext    : '.css',
+                    expand : true,
                     flatten: false
                 }]
             }
@@ -144,24 +144,24 @@ module.exports = function (grunt) {
          * @link https://www.npmjs.org/package/grunt-contrib-cssmin
          */
         cssmin: {
-            main: {
+            main          : {
                 options: {
                     keepSpecialComments: 0
                 },
-                expand: true,
-                cwd: pathCSS + '/',
-                src: ['*.css', '!*.min.css'],
-                dest: pathCSS + '/',
-                ext: '.min.css'
+                expand : true,
+                cwd    : pathCSS + '/',
+                src    : ['*.css', '!*.min.css'],
+                dest   : pathCSS + '/',
+                ext    : '.min.css'
             },
             options_fields: {
                 options: {
                     keepSpecialComments: 0
                 },
-                files: [{
-                    src: [pathCSS_options_fields + '/**/*.css'],
-                    ext: '.min.css',
-                    expand: true,
+                files  : [{
+                    src    : [pathCSS_options_fields + '/**/*.css'],
+                    ext    : '.min.css',
+                    expand : true,
                     flatten: false
                 }]
             }
@@ -170,21 +170,21 @@ module.exports = function (grunt) {
         makepot: {
             target: {
                 options: {
-                    mainFile: pkgName + '.php',                     // Main project file.
-                    potHeaders: {
-                        poedit: true,                 // Includes common Poedit headers.
+                    mainFile       : pkgName + '.php',                     // Main project file.
+                    potHeaders     : {
+                        poedit                 : true,                 // Includes common Poedit headers.
                         'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
                     },                                // Headers to add to the generated POT file.
-                    processPot: function (pot, options) {
+                    processPot     : function (pot) {
                         pot.headers['report-msgid-bugs-to'] = 'http://www.wpglobus.com/pg/contact-us/';
                         pot.headers['language-team'] = 'The WPGlobus Team <support@wpglobus.com>';
                         pot.headers['last-translator'] = pot.headers['language-team'];
                         delete pot.headers['x-generator'];
                         return pot;
                     },
-                    type: 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
+                    type           : 'wp-plugin',                // Type of project (wp-plugin or wp-theme).
                     updateTimestamp: true,             // Whether the POT-Creation-Date should be updated without other changes.
-                    updatePoFiles: false              // Whether to update PO files in the same directory as the POT file.
+                    updatePoFiles  : false              // Whether to update PO files in the same directory as the POT file.
                 }
             }
         },
@@ -193,37 +193,37 @@ module.exports = function (grunt) {
             tx_push_s: {
                 cmd: 'tx push -s'
             },
-            tx_pull: { // Pull Transifex translation - grunt exec:tx_pull
+            tx_pull  : { // Pull Transifex translation - grunt exec:tx_pull
                 cmd: 'tx pull -a -f --mode=translator' // Change the percentage with --minimum-perc=value
             }
         },
 
         replace: {
             readme_md: {
-                overwrite: true,
-                src: ['README.md'],
+                overwrite   : true,
+                src         : ['README.md'],
                 replacements: [
                     {
                         from: '![Multilingual WooCommerce store powered by [WooCommerce WPGlobus](http://www.wpglobus.com/shop/extensions/woocommerce-wpglobus/).](https://ps.w.org/wpglobus/assets/screenshot-8.png)',
-                        to: '![Multilingual WooCommerce store powered by WooCommerce WPGlobus.](https://ps.w.org/wpglobus/assets/screenshot-8.png)'
+                        to  : '![Multilingual WooCommerce store powered by WooCommerce WPGlobus.](https://ps.w.org/wpglobus/assets/screenshot-8.png)'
                     },
                     {
                         from: '# WPGlobus - Multilingual Everything! #',
-                        to: '[![Latest Stable Version](https://poser.pugx.org/wpglobus/wpglobus/v/stable)](https://packagist.org/packages/wpglobus/wpglobus) [![Total Downloads](https://poser.pugx.org/wpglobus/wpglobus/downloads)](https://packagist.org/packages/wpglobus/wpglobus) [![Latest Unstable Version](https://poser.pugx.org/wpglobus/wpglobus/v/unstable)](https://packagist.org/packages/wpglobus/wpglobus) [![License](https://poser.pugx.org/wpglobus/wpglobus/license)](https://packagist.org/packages/wpglobus/wpglobus)\n\n# WPGlobus - Multilingual Everything! #'
+                        to  : '[![Latest Stable Version](https://poser.pugx.org/wpglobus/wpglobus/v/stable)](https://packagist.org/packages/wpglobus/wpglobus) [![Total Downloads](https://poser.pugx.org/wpglobus/wpglobus/downloads)](https://packagist.org/packages/wpglobus/wpglobus) [![Latest Unstable Version](https://poser.pugx.org/wpglobus/wpglobus/v/unstable)](https://packagist.org/packages/wpglobus/wpglobus) [![License](https://poser.pugx.org/wpglobus/wpglobus/license)](https://packagist.org/packages/wpglobus/wpglobus)\n\n# WPGlobus - Multilingual Everything! #'
                     }
                 ]
             },
-            version: {
-                overwrite: true,
-                src: [pkgName + '.php'],
+            version  : {
+                overwrite   : true,
+                src         : [pkgName + '.php'],
                 replacements: [
                     {
                         from: / \* Version: .+/,
-                        to: ' * Version: <%= pkg.version %>'
+                        to  : ' * Version: <%= pkg.version %>'
                     },
                     {
                         from: /define\( 'WPGLOBUS_VERSION', '[^']+' \);/,
-                        to: "define( 'WPGLOBUS_VERSION', '<%= pkg.version %>' );"
+                        to  : "define( 'WPGLOBUS_VERSION', '<%= pkg.version %>' );"
                     }
                 ]
             }
@@ -234,14 +234,14 @@ module.exports = function (grunt) {
          * @link https://github.com/gruntjs/grunt-contrib-watch
          */
         watch: {
-            files: [
+            files  : [
                 'Gruntfile.js',
                 pathCSS + '/*.less',
                 pathJS + '/*.js',
                 pathCSS_options_fields + '/**/*.less',
                 pathJS_options_fields + '/**/*.js'
             ],
-            tasks: ['less', 'cssmin', 'uglify'],
+            tasks  : ['less', 'cssmin', 'uglify'],
             options: {
                 spawn: false
             }
@@ -249,23 +249,28 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('po', 'Merge POT into individual PO files', function () {
-        var execSync = require('child_process').execSync;
-        var poFilePaths = grunt.file.expand('languages/*.po');
+        var execSync = require('child_process').execSync,
+            poFilePaths = grunt.file.expand('languages/*.po');
         poFilePaths.forEach(function (poFile) {
-            var moFile = poFile.replace(/\.po$/, '.mo');
             grunt.log.writeln("Making PO: " + poFile);
             execSync('msgmerge -v --backup=none --no-fuzzy-matching --update ' + poFile + ' ' + potFile);
         });
     });
 
     grunt.registerTask('mo', 'Compile PO to MO files', function () {
-        var execSync = require('child_process').execSync;
-        var poFilePaths = grunt.file.expand('languages/*.po');
+        var execSync = require('child_process').execSync,
+            poFilePaths = grunt.file.expand('languages/*.po');
         poFilePaths.forEach(function (poFile) {
             var moFile = poFile.replace(/\.po$/, '.mo');
             grunt.log.writeln("Making MO: " + moFile);
             execSync('msgfmt -v -o ' + moFile + ' ' + poFile);
         });
+    });
+
+    grunt.registerTask('cpzu', 'Install dependencies with Composer', function () {
+        var execSync = require('child_process').execSync,
+            pathComposer = '/cygwin64/usr/local/bin/composer';
+        execSync('php ' + pathComposer + ' update --no-ansi --no-autoloader');
     });
 
     grunt.registerTask('pomo', [
@@ -279,6 +284,7 @@ module.exports = function (grunt) {
     grunt.registerTask('readme_md', ['wp_readme_to_markdown', 'replace:readme_md']);
 
     grunt.registerTask('dist', [
+        'cpzu',
         'replace:version',
         'pomo',
         'readme_md',
