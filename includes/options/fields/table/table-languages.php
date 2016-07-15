ed1bdcdf4d630e760350e1ab0ef6e15dece3fffd
Alex Gor <alex.wpstarter@gmail.com>
2016-07-15 18:10:45 +0700

    [class LanguagesTable] improvements to find out what installed languages

--- includes/options/fields/table/table-languages.php
+++ includes/options/fields/table/table-languages.php
@@ -375,6 +375,21 @@ class LanguagesTable extends WP_List_Table {
 			'<input type="checkbox" name="item[]" value="%s" />', $item['ID']
 		);
 	}
+	
+	/**
+	 * Define function for add item actions by name 'wpglobus_en_language_name'
+	 * @since 1.5.10
+	 *
+	 * @param  $item array
+	 *
+	 * @return string
+	 */	
+	function column_wpglobus_en_language_name( $item ) {
+		if ( in_array( $item[ 'wpglobus_code' ], WPGlobus::Config()->enabled_languages ) ) {
+			return $item[ 'wpglobus_en_language_name' ] . ' (<b>' . __( 'Installed', 'wpglobus' ) . '</b>)';
+		}			
+		return $item[ 'wpglobus_en_language_name' ];
+	}	
 
 	/**
 	 * Generate the table navigation above or below the table
@@ -401,6 +416,25 @@ class LanguagesTable extends WP_List_Table {
 	<?php
 
 	}
+	
+	/**
+	 * Generates content for a single row of the table
+	 *
+	 * @since 1.5.10
+	 * @access public
+	 *
+	 * @param object $item The current item
+	 */
+	public function single_row( $item ) {
+		$style = '';
+		if ( in_array( $item[ 'wpglobus_code' ], WPGlobus::Config()->enabled_languages ) ) {
+			$style = ' style="background-color:#0ff;" ';
+		}	
+		
+		echo '<tr' . $style . '>';
+		$this->single_row_columns( $item );
+		echo '</tr>';
+	}	
 
 }
 
