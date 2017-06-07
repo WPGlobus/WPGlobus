45a6c72608efff2dd149be619ec2d299321c1a69
Gregory K <gregory@tiv.net>
2017-06-07 14:25:54 -0400

    Added: (Core) redirect based on the browser language at first visit - fixes.

--- includes/class-wpglobus-redirect.php
+++ includes/class-wpglobus-redirect.php
@@ -1,63 +1,69 @@
 <?php
 /**
  * @package WPGlobus
- * @since 1.8
+ * @since   1.8
  */
 
 /**
  * Class WPGlobus_Redirect
  */
 class WPGlobus_Redirect {
-	
-		/**
-		 * Constructor.
-		 */
-		public static function construct() {
-			add_action( 'init', array( __CLASS__, 'on__init' ), 1 );
-		}
-		
-		/**
-		 * Init action.
-		 */
-		public static function on__init() {
-			
-			if ( empty( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) ) {
-				return;
-			}
-		
-			$cookiename = WPGLOBUS::_COOKIE;
-		
-			if( ! isset( $_COOKIE[$cookiename] ) ) {
-				/**
-				 * First visit.
-				 */
-				$browser_language = substr( $_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2 );
-				setcookie( $cookiename,  $browser_language, time()+3600*24*365, '/' );
-				self::redirect( $browser_language );
-				return;
-			}
-			
-		}		
-		
-		/**
-		 * Redirect to specified language.
-		 */
-		public static function redirect( $language ) {
 
-			if ( $language == WPGlobus::Config()->default_language ) {
-				return;
-			}
-			
-			if ( ! in_array( $language, WPGlobus::Config()->enabled_languages ) ) {
-				return;
-			}
-			
-			$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-			$url = WPGlobus_Utils::localize_url( $url, $language );			
-			wp_redirect( $url ); 
-			exit;			
-			
+	/**
+	 * Constructor.
+	 */
+	public static function construct() {
+		add_action( 'init', array( __CLASS__, 'on__init' ), 1 );
+	}
+
+	/**
+	 * Init action.
+	 */
+	public static function on__init() {
+
+		if ( empty( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) ) {
+			// No language information in browser.
+			return;
 		}
-		
+
+		$cookie_name = WPGlobus::_COOKIE;
+
+		if ( ! isset( $_COOKIE[ $cookie_name ] ) ) {
+			/**
+			 * First visit.
+			 */
+			$browser_language = substr( $_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2 );
+			/* @noinspection SummerTimeUnsafeTimeManipulationInspection */
+			setcookie( $cookie_name, $browser_language, time() + 3600 * 24 * 365, '/' );
+			self::redirect( $browser_language );
+
+			return;
+		}
+
+	}
+
+	/**
+	 * Redirect to specified language.
+	 *
+	 * @param string $language
+	 */
+	public static function redirect( $language ) {
+
+		if ( $language === WPGlobus::Config()->language ) {
+			// Already in that language.
+			return;
+		}
+
+		if ( ! in_array( $language, WPGlobus::Config()->enabled_languages, true ) ) {
+			// No such language.
+			return;
+		}
+
+		// Convert the current URL to the requested language and redirect.
+		$current_url = WPGlobus_Utils::current_url();
+		$redirect_to = WPGlobus_Utils::localize_url( $current_url, $language );
+		wp_redirect( $redirect_to );
+		exit;
+
+	}
 }
-# --- EOF
