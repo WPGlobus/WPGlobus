b05816b586d8b98e6f715cf996b05ec8ef7ed5c4
tivnet <tivgit2311>
2018-01-12 23:25:55 -0500

    Escaping OK: class-wpglobus-admin-recommendations.php

--- includes/admin/recommendations/class-wpglobus-admin-recommendations.php
+++ includes/admin/recommendations/class-wpglobus-admin-recommendations.php
@@ -1,20 +1,31 @@
 <?php
 /**
  * WPGlobus Recommendations.
- * @since 1.8.7
+ *
+ * @package WPGlobus\Admin
+ *
+ * @since   1.8.7
  */
 
+/**
+ * Class Admin Recommendations.
+ */
 class WPGlobus_Admin_Recommendations {
 
+	/**
+	 * Setup actions and filters.
+	 */
 	public static function setup_hooks() {
 		add_filter( 'woocommerce_general_settings', array( __CLASS__, 'for_woocommerce' ) );
 		add_filter( 'wpglobus_edit_slug_box', array( __CLASS__, 'wpg_plus_slug' ) );
 	}
 
 	/**
+	 * Recommendations for WooCommerce.
+	 *
 	 * @internal
 	 *
-	 * @param array $settings
+	 * @param array $settings Passed by WooCommerce.
 	 *
 	 * @return array
 	 */
@@ -40,7 +51,7 @@ class WPGlobus_Admin_Recommendations {
 			$desc  =
 				'<h2><span class="wp-ui-notification" style="padding:10px 20px;">' .
 				'<span class="dashicons dashicons-admin-site"></span> ' .
-				__( 'WPGlobus Recommends:', 'wpglobus' ) .
+				esc_html__( 'WPGlobus Recommends:', 'wpglobus' ) .
 				'</span></h2>';
 
 			self::add_wc_section( $settings, $id, $title, $desc );
@@ -49,7 +60,7 @@ class WPGlobus_Admin_Recommendations {
 		if ( $need_to_recommend_wpg_wc ) {
 			$url   = WPGlobus_Utils::url_wpglobus_site() . 'product/woocommerce-wpglobus/';
 			$id    = 'wpglobus-recommend-wpg-wc';
-			$title = '&bull; ' . __( 'WPGlobus for WooCommerce', 'wpglobus' );
+			$title = '&bull; ' . esc_html__( 'WPGlobus for WooCommerce', 'wpglobus' );
 			$desc  =
 				'<p class="wp-ui-text-notification">' .
 				'<strong>' .
@@ -60,7 +71,7 @@ class WPGlobus_Admin_Recommendations {
 				'<strong>' .
 				esc_html__( 'Get it now:', 'wpglobus' ) . ' ' .
 				'</strong>' .
-				'<a href="' . $url . '">' . $url . '</a>' .
+				'<a href="' . esc_url( $url ) . '">' . esc_html( $url ) . '</a>' .
 				'</p>';
 			self::add_wc_section( $settings, $id, $title, $desc );
 		}
@@ -92,23 +103,32 @@ class WPGlobus_Admin_Recommendations {
 	/**
 	 * Generic WC option section consisting of one block of text only.
 	 *
-	 * @param array $settings Array of WC settings, passed by reference.
-	 * @param string $id Section ID, must be unique.
-	 * @param string $title Section title, no HTML.
-	 * @param string $desc The text to display, HTML is allowed.
+	 * @param array  $settings Array of WC settings, passed by reference.
+	 * @param string $id       Section ID, must be unique.
+	 * @param string $title    Section title, no HTML.
+	 * @param string $desc     The text to display, HTML is allowed.
 	 *
 	 * @return void
 	 */
 	protected static function add_wc_section( &$settings, $id, $title, $desc ) {
 		$settings[] =
-			array( 'type' => 'title', 'id' => $id, 'title' => $title, 'desc' => $desc );
+			array(
+				'type'  => 'title',
+				'id'    => $id,
+				'title' => $title,
+				'desc'  => $desc,
+			);
 
 		$settings[] =
-			array( 'type' => 'sectionend', 'id' => $id );
+			array(
+				'type' => 'sectionend',
+				'id'   => $id,
+			);
 	}
 
 	/**
 	 * Recommend WPGlobus Plus to edit permalinks.
+	 *
 	 * @since 1.9.6
 	 */
 	public static function wpg_plus_slug() {
@@ -118,23 +138,23 @@ class WPGlobus_Admin_Recommendations {
 
 		if ( ! is_plugin_active( 'wpglobus-plus/wpglobus-plus.php' ) ) {
 			$url = WPGlobus_Utils::url_wpglobus_site() . 'product/wpglobus-plus/#slug';
-			echo $container_start;
+			echo $container_start; // WPCS: XSS ok.
 			esc_html_e( 'Translate permalinks with our premium add-on, WPGlobus Plus!', 'wpglobus' );
 			echo ' ';
 			esc_html_e( 'Check it out:', 'wpglobus' );
 			echo ' ';
-			echo '<a href="' . esc_url( $url ) . '" target="_blank">' . $url . '</a>';
-			echo $container_end;
+			echo '<a href="' . esc_url( $url ) . '" target="_blank">' . esc_html( $url ) . '</a>';
+			echo $container_end; // WPCS: XSS ok.
 		} elseif ( ! class_exists( 'WPGlobusPlus_Slug', false ) ) {
 			$url = admin_url( 'admin.php' ) . '?page=' . WPGlobusPlus::WPGLOBUS_PLUS_OPTIONS_PAGE . '&tab=modules';
-			echo $container_start;
+			echo $container_start; // WPCS: XSS ok.
 			esc_html_e( 'To translate permalinks, please activate the module Slug.', 'wpglobus' );
 			echo ' ';
-			echo '<a href="' . esc_url( $url ) . '" target="_blank">' .
-			     /// Do not translate
-			     esc_html__( 'Go to WPGlobus Plus Options page', 'wpglobus-plus' ) .
-			     '.</a>';
-			echo $container_end;
+			/// Do not translate
+			$msg = __( 'Go to WPGlobus Plus Options page', 'wpglobus-plus' );
+
+			echo '<a href="' . esc_url( $url ) . '" target="_blank">' . esc_html( $msg ) . '.</a>';
+			echo $container_end; // WPCS: XSS ok.
 		}
 	}
 }
