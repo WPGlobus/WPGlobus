f3012cdac1c8ee5b55573ed3a7b80f789ec492b2
Gregory Karpinsky <gregory@tiv.net>
2018-03-08 22:10:14 -0500

    [Options2] field_wpglobus_info.php cleanup

--- includes/options/fields2/wpglobus_info/field_wpglobus_info.php
+++ includes/options/fields2/wpglobus_info/field_wpglobus_info.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * wpglobus_info
+ */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
@@ -15,62 +18,47 @@ if ( ! class_exists( 'WPGlobusOptions_wpglobus_info' ) ) {
 
 		/**
 		 * Field Constructor.
-		 * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
 		 *
-		 * @since       1.0.0
-		 * @access      public
-		 * @param array  $field
-		 * @param string $value
-		 * @param        $parent
+		 * @param array $field Field attributes.
 		 */
-		public function __construct( $field = array(), $value = '' ) {
-			
-			$this->field  = $field;
-			if ( ! empty($field['value']) ) {
-				$this->value = $field['value'];
-			} else {
-				$this->value = $value;
-			}
-			
-			$this->render();
-			
+		public function __construct( $field ) {
+
+			$this->render( $field );
 		}
 
 		/**
-		 * Field Render Function.
-		 * Takes the vars and outputs the HTML for the field in the settings.
+		 * Render the field.
 		 *
-		 * @access      public
-		 * @return      void
+		 * @param array $field Field attributes.
 		 */
-		public function render() {
+		public function render( $field ) {
 
-			ob_start();
-			$class = empty($this->field['class']) ? '' : ' '.$this->field['class']; ?>
-			<div 
-				id="wpglobus-options-<?php echo $this->field['id']; ?>" 
-				class="wpglobus-options-field wpglobus-options-field-<?php echo $this->field['id']; ?> wpglobus-options-field-<?php echo $this->field['type']; ?><?php echo $class; ?>" 
-				data-id="<?php echo $this->field['id']; ?>" 
-				data-type="<?php echo $this->field['type']; ?>">
-					<?php if ( ! empty( $this->field['title'] ) ) {	?>
-						<p class="title"><?php echo $this->field['title']; ?></p>
-					<?php }	?>		
-					<?php if ( ! empty( $this->field['subtitle'] ) ) {	?>
-						<p class="subtitle"><?php echo $this->field['subtitle']; ?></p>
-					<?php }	?>
-					<?php if ( ! empty( $this->field['html'] ) ) {	?>
-						<?php echo $this->field['html']; ?>
-					<?php }	?>	
-					<?php if ( ! empty( $this->field['desc'] ) ) {	?>
-						<p class="description"><?php echo $this->field['desc']; ?></p>
-					<?php }	?>	
-			</div>	
+			$class = empty( $field['class'] ) ? '' : ' ' . $field['class'];
+			?>
+			<div id="wpglobus-options-<?php echo esc_attr( $field['id'] ); ?>"
+					class="wpglobus-options-field wpglobus-options-field-<?php echo esc_attr( $field['id'] ); ?> wpglobus-options-field-<?php echo esc_attr( $field['type'] ); ?><?php echo esc_attr( $class ); ?>">
+				<?php if ( ! empty( $field['title'] ) ) { ?>
+					<p class="title"><?php echo esc_html( $field['title'] ); ?></p>
+				<?php } ?>
+				<?php if ( ! empty( $field['subtitle'] ) ) { ?>
+					<p class="subtitle"><?php echo esc_html( $field['subtitle'] ); ?></p>
+				<?php } ?>
+				<?php if ( ! empty( $field['html'] ) ) { ?>
+					<?php echo wp_kses_post( $field['html'] ); ?>
+				<?php } ?>
+				<?php if ( ! empty( $field['desc'] ) ) { ?>
+					<p class="description"><?php echo wp_kses_post( $field['desc'] ); ?></p>
+				<?php } ?>
+			</div>
 			<div style="clear:both;"></div>
 			<?php
-			echo ob_get_clean();
 
 		}
 	}
 }
 
-new WPGlobusOptions_wpglobus_info($field);
+/**
+ * @global array $field
+ * @see WPGlobus_Options::pageOptions
+ */
+new WPGlobusOptions_wpglobus_info( $field );
