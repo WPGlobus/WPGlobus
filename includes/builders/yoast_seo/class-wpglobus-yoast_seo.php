0270a71acb0ae1ca78ddcecfd5c1bceb894ebe0d
Alex Gor <alex.wpstarter@gmail.com>
2018-08-20 18:08:09 +0700

    [builders] added file: class-wpglobus-yoast_seo.php

---
+++ includes/builders/yoast_seo/class-wpglobus-yoast_seo.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * File: class-wpglobus-yoast_seo.php
+ *
+ * @package WPGlobus\Builders\YoastSeo
+ * @author Alex Gor(alexgff)
+ */
+
+ 
+if ( ! class_exists( 'WPGlobus_Yoast_SEO' ) ) :
+
+	/**
+	 * Class WPGlobus_Yoast_SEO.
+	 */
+	class WPGlobus_Yoast_SEO extends WPGlobus_Builder {
+		
+		/**
+		 * Constructor.
+		 */
+		public function __construct()  {
+			parent::__construct('yoast_seo');
+		}
+
+	}
+
+endif;
+
+# --- EOF
