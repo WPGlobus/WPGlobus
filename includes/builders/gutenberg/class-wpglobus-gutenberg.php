2240dde346d93a70553331626e70e87b652f6345
Alex Gor <alex.wpstarter@gmail.com>
2018-09-01 12:59:01 +0700

    [gutenberg] added language switcher box for 'post-new.php' page.

--- includes/builders/gutenberg/class-wpglobus-gutenberg.php
+++ includes/builders/gutenberg/class-wpglobus-gutenberg.php
@@ -17,7 +17,7 @@ class WPGlobus_Gutenberg extends WPGlobus_Builder {
 	public function __construct() {
 		
 		parent::__construct('gutenberg');
-		
+
 		if ( is_admin() ) {
 			
 			/**
@@ -30,7 +30,7 @@ class WPGlobus_Gutenberg extends WPGlobus_Builder {
 			 * Add 'wpglobus-language' hidden field.
 			 */
 			add_action( 'add_meta_boxes', array( $this, 'on__add_meta_box' ) );
-			
+
 			add_action( 'admin_enqueue_scripts', array(
 				$this,
 				'on__enqueue_scripts'
@@ -69,7 +69,7 @@ class WPGlobus_Gutenberg extends WPGlobus_Builder {
 	 * @since 2.0
 	 * @return string
 	 */
-	private function get_switcher_box() {
+	private function get_switcher_box($page) {
 		
 		$query_string = explode('&', $_SERVER['QUERY_STRING']);
 		
@@ -86,44 +86,50 @@ class WPGlobus_Gutenberg extends WPGlobus_Builder {
 				'post.php?'.$query 
 			)
 		);
+
+		$_box_style = 'position:absolute;top:15px;left:10px;z-index:100;';
+		if ( file_exists( WPGlobus::Config()->flag_path['big'].WPGlobus::Config()->flag[$this->language] ) ) {
+			$_flag_img = WPGlobus::Config()->flag_urls['big'].WPGlobus::Config()->flag[$this->language];
+			$_height = 'height="25px"';
+			$_width  = 'width="25px"';
+			$_flag_style  = 'style="border: 1px solid #bfbfbf;border-radius: 25px;"';
+		} else {
+			$_flag_img = WPGlobus::Config()->flags_url.WPGlobus::Config()->flag[$this->language];
+			$_height = '';
+			$_width = '';
+			$_flag_style = 'style="margin-top:5px;"';
+			$_box_style .= 'margin-top:3px;';
+		}
 		
-		if ( false ) {
+		if ( 'post-new.php' == $page ) {
+		
 			ob_start();
 			?>
-			<div style="position:absolute;top:20px;left:10px;z-index:100;" class="wpglobus-gutenberg-selector-box">
-				<a href="<?php echo str_replace('{{language}}', $this->language, $url); ?>" style="" class="wpglobus-gutenberg-selector" data-language="<?php echo $this->language; ?>">
-					<img src="<?php echo WPGlobus::Config()->flags_url.WPGlobus::Config()->flag[$this->language]; ?>" />&nbsp;<?php echo WPGlobus::Config()->en_language_name[$this->language]; ?>
-				</a>
-				<ul class="wpglobus-gutenberg-selector-dropdown" style="display:none;position:fixed;width:80px;border-left:1px solid #ddd;border-right:1px solid #ddd;background-color:#eee;margin:5px 0 0;list-style-type:none;">
-					<?php foreach( WPGlobus::Config()->enabled_languages as $lang ) :	?>
-						<?php if ( $lang == $this->language ) { continue; }	?>
-						<li class="item" style="text-align:left;cursor:pointer;border-bottom:1px solid #ddd;margin:0;height:25px;padding:5px 0 5px 5px;" data-language="<?php echo $lang; ?>">
-							<a href="<?php echo str_replace('{{language}}', $lang, $url); ?>">
-								<img src="<?php echo WPGlobus::Config()->flags_url.WPGlobus::Config()->flag[$lang]; ?>" />&nbsp;<?php echo WPGlobus::Config()->en_language_name[$lang]; ?>
-							</a>
-						</li>
-					<?php endforeach; ?>
+			<div style="<?php echo $_box_style; ?>" class="wpglobus-gutenberg-selector-box">
+				<div style="display:grid;grid-template-columns:50% 50%;place-items:center;grid-gap:0;">
+					<a style="text-decoration:none;cursor:text;" onclick="return false; 
+						href="#" style="" class="wpglobus-gutenberg-selector" data-language="<?php echo $this->language; ?>">
+						<img <?php echo $_height . $_width; ?>  
+							<?php echo $_flag_style; ?>  
+							src="<?php echo $_flag_img ?>" />
+					</a>
+					<a style="text-decoration:none;cursor:text;" onclick="return false;"
+						href="#" style="" class="wpglobus-gutenberg-selector" data-language="<?php echo $this->language; ?>">
+					&nbsp;<span><?php echo WPGlobus::Config()->en_language_name[$this->language]; ?></span>
+					</a>
+				</div>
+				<ul class="wpglobus-gutenberg-selector-dropdown" style="display:none;position:fixed;width:300px;border-left:1px solid #ddd;border-right:1px solid #ddd;background-color:#eee;margin:5px 0 0;list-style-type:none;">
+					<li class="item" style="text-align:left;border-bottom:1px solid #ddd;margin:0 auto;height:25px;padding:3px 0 0 10px;">
+						<?php _e('Save draft or publish post and reload page.', 'wpglobus'); ?>
+					</li>
 				</ul>
-			</div>
+			</div>			
 			<?php
 			$out = ob_get_contents();
-			ob_end_clean();
+			ob_end_clean();			
 			
-		} else {
+		} else if ( 'post.php' == $page ) {
 
-			$_box_style = 'position:absolute;top:15px;left:10px;z-index:100;';
-			if ( file_exists( WPGlobus::Config()->flag_path['big'].WPGlobus::Config()->flag[$this->language] ) ) {
-				$_flag_img = WPGlobus::Config()->flag_urls['big'].WPGlobus::Config()->flag[$this->language];
-				$_height = 'height="25px"';
-				$_width  = 'width="25px"';
-				$_flag_style  = 'style="border: 1px solid #bfbfbf;border-radius: 25px;"';
-			} else {
-				$_flag_img = WPGlobus::Config()->flags_url.WPGlobus::Config()->flag[$this->language];
-				$_height = '';
-				$_width = '';
-				$_flag_style = 'style="margin-top:5px;"';
-				$_box_style .= 'margin-top:3px;';
-			}
 			ob_start();
 			?>
 			<div style="<?php echo $_box_style; ?>" class="wpglobus-gutenberg-selector-box">
@@ -155,6 +161,7 @@ class WPGlobus_Gutenberg extends WPGlobus_Builder {
 			ob_end_clean();
 			
 		}
+		
 		return $out;
 
 	}
@@ -175,15 +182,15 @@ class WPGlobus_Gutenberg extends WPGlobus_Builder {
 		
 		global $pagenow;
 		
-		if ( 'post.php' != $pagenow ) {
+		if ( ! in_array( $pagenow, array('post.php', 'post-new.php') ) ) {
 			return;
-		}
-		
+		} 		
+
 		if ( isset( $_GET['classic-editor'] ) ) {
 			return;
 		}
 		
-		$tabs = $this->get_switcher_box();
+		$tabs = $this->get_switcher_box($pagenow);
 	
 		wp_register_script(
 			'wpglobus-gutenberg',
@@ -201,6 +208,7 @@ class WPGlobus_Gutenberg extends WPGlobus_Builder {
 				'versionGutenberg' 	=> GUTENBERG_VERSION,
 				'tabs'  			=> $tabs,
 				'language'  		=> $this->language,
+				'pagenow'  			=> $pagenow,
 				'defaultLanguage' 	=> WPGlobus::Config()->default_language
 			)
 		);
