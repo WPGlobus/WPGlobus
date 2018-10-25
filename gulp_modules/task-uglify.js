27770024463eacbbd11f4a806904b04ad5e3c374
Gregory Karpinsky <tiv.net.inc@gmail.com>
2018-10-25 08:55:10 -0400

    Fix: missing min.js

--- gulp_modules/task-uglify.js
+++ gulp_modules/task-uglify.js
@@ -1,12 +1,34 @@
 "use strict";
 
-module.exports = function () {
+module.exports = function (cb) {
     var gulp = require("gulp");
     var cfg = require("./cfg.json");
     var pkg = require('../package.json');
     var uglify = require("gulp-uglify");
     var rename = require("gulp-rename");
     var print = require('gulp-print').default;
+
+    gulp
+        .src([
+            "includes/builders/assets/*.js",
+            "!includes/**/*.min.js"
+        ])
+        .pipe(uglify())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest("includes/builders/assets"))
+        .pipe(print())
+    ;
+
+    gulp
+        .src([
+            "includes/builders/gutenberg/assets/js/*.js",
+            "!includes/**/*.min.js"
+        ])
+        .pipe(uglify())
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest("includes/builders/gutenberg/assets/js"))
+        .pipe(print())
+    ;
 
     return gulp
         .src([cfg.path.js + "/**/*.js", "!" + cfg.path.js + "/**/*.min.js"])
@@ -16,3 +38,13 @@ module.exports = function () {
         .pipe(print())
         ;
 };
+
+
+// var pump = require('pump');
+// pump([
+//     gulp.src("includes/builders/assets/**/*.js"),
+//     uglify(),
+//     rename({suffix: ".min"}),
+//     print()
+// ], cb)
+// ;
