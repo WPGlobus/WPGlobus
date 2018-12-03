fc5c23598ae1dae62dc5dc3c4e08fcf569b9b56a
Alex Gor <alex.wpstarter@gmail.com>
2018-12-03 12:55:59 +0700

    [options] updated info for compatibility tab

--- includes/options/templates/compatibility.php
+++ includes/options/templates/compatibility.php
@@ -19,6 +19,21 @@ if ( ! function_exists( 'is_plugin_active' ) ) {
 
 $add_ons = WPGlobus_Builders::get_addons();
 
+if ( ! empty( $add_ons['gutenberg'] ) ) {
+	
+	/**
+	 * We have Gutenberg in core since WP 5.0.
+	 * @since 2.0 
+	 */
+	global $wp_version; 
+	if ( version_compare( $wp_version, '4.9.99', '>' ) ) {
+		$add_ons['gutenberg']['context'] = 'core';
+	} else {
+		$add_ons['gutenberg']['context'] = '';
+	}
+	
+}
+
 $compatibility = '<h3>' . esc_html__( 'List of supported add-ons', 'wpglobus' ) . ':</h3>';
 
 $compatibility .= '<table id="wpglobus-options-compatibility">';
@@ -33,25 +48,36 @@ $compatibility .= '</tr>';
 $compatibility .= '</thead>';
 
 $compatibility .= '<tbody>';
-foreach ( $add_ons as $add_on ) {
-
+foreach ( $add_ons as $id=>$add_on ) {
+	
 	$_version = '';
 	$_status  = '';
-	$_file    = WP_PLUGIN_DIR . '/' . $add_on['path'];
-	if ( file_exists( $_file ) ) {
-
-		$_fd      = get_file_data( $_file, array( 'version' => 'Version' ) );
-		$_version = $_fd['version'];
-
-		if ( is_plugin_active( $add_on['path'] ) ) {
-			$_status = esc_html__( 'Active', 'wpglobus' );
-		} else {
-			$_status = esc_html__( 'Installed, inactive', 'wpglobus' );
-		}
+	
+	if ( 'gutenberg' == $id && 'core' == $add_on['context'] ) {
+		
+		$_version = $wp_version;
+		$_status  = esc_html__( 'In core', 'wpglobus' );
+		$add_on['supported_min_version'] = '';
+		
 	} else {
-		$_status = esc_html__( 'Not installed', 'wpglobus' );
-	}
 
+		$_file    = WP_PLUGIN_DIR . '/' . $add_on['path'];
+		if ( file_exists( $_file ) ) {
+
+			$_fd      = get_file_data( $_file, array( 'version' => 'Version' ) );
+			$_version = $_fd['version'];
+
+			if ( is_plugin_active( $add_on['path'] ) ) {
+				$_status = esc_html__( 'Active', 'wpglobus' );
+			} else {
+				$_status = esc_html__( 'Installed, inactive', 'wpglobus' );
+			}
+		} else {
+			$_status = esc_html__( 'Not installed', 'wpglobus' );
+		}
+		
+	}
+	
 	$_stage = '';
 	if ( empty( $add_on['stage'] ) ) {
 		$_stage = 'production';
