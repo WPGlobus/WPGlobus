4dd3c013c90340bade36818840517c7d7b78b66c
Alex Gor <alex.wpstarter@gmail.com>
2019-01-08 17:16:49 +0700

    [core] prevent to the handling array and object.

--- includes/class-wpglobus.php
+++ includes/class-wpglobus.php
@@ -3275,10 +3275,11 @@ class WPGlobus {
 	 */
 	public static function add_locale_marks( $text, $language ) {
 		/**
-		 * @since 1.9.17
-		 * Add marks for string only.
+		 * @since 1.9.17 Add marks for string only.
+		 * @since 2.1.5  Prevent to the handling array and object.
 		 */
-		if ( ! is_string( $text ) ) {
+		//if ( ! is_string( $text ) ) {
+		if ( is_array( $text ) || is_object( $text ) ) {
 			return $text;
 		}
 
