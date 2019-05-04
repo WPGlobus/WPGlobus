0b78bed49b767fe69175be985cf4d2de696460dd
Gregory Karpinsky <tiv.net.inc@gmail.com>
2019-05-04 09:56:54 -0400

    Git-to-SVN

--- gulp_modules/task-pot.js
+++ gulp_modules/task-pot.js
@@ -1,23 +1,23 @@
 "use strict";
 
 module.exports = function () {
-    var gulp = require("gulp");
-    var wpPot = require("gulp-wp-pot");
-    var cfg = require("./cfg.json");
-    var pkg = require('../package.json');
-    var log = require('fancy-log');
-    var potFile = cfg.path.languages + "/" + pkg.name + ".pot";
+	var gulp = require("gulp");
+	var wpPot = require("gulp-wp-pot");
+	var cfg = require("./cfg.json");
+	var pkg = require('../package.json');
+	var log = require('fancy-log');
+	var potFile = cfg.path.languages + "/" + pkg.name + ".pot";
 
-    log.info(potFile);
-    return gulp.src(["**/*.php", "!**/*Test.php", "!vendor/**/"])
-        .pipe(wpPot({
-            domain: cfg.text_domain,
-            package: pkg.title,
-            bugReport: cfg.bugReport,
-            headers: false,
-            lastTranslator: pkg.author,
-            relativeTo: ".",
-            metadataFile: pkg.name + ".php"
-        }))
-        .pipe(gulp.dest(potFile));
+	log.info(potFile);
+	return gulp.src(["**/*.php", "!**/*Test.php", "!vendor/**/", "!wpsvn/**/"])
+		.pipe(wpPot({
+			domain: cfg.text_domain,
+			package: pkg.title,
+			bugReport: cfg.bugReport,
+			headers: false,
+			lastTranslator: pkg.author,
+			relativeTo: ".",
+			metadataFile: pkg.name + ".php"
+		}))
+		.pipe(gulp.dest(potFile));
 };
