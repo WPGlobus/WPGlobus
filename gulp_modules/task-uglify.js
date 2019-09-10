d7b1ff96007c49d5154bc954d92f0a2be6c2740c
Gregory Karpinsky <tiv.net.inc@gmail.com>
2019-09-09 21:58:23 -0400

    Gulp: added Babel

--- gulp_modules/task-uglify.js
+++ gulp_modules/task-uglify.js
@@ -1,53 +1,59 @@
 "use strict";
 
 module.exports = function (cb) {
-    var gulp = require("gulp");
-    var cfg = require("./cfg.json");
-    var pkg = require('../package.json');
-    var uglify = require("gulp-uglify");
-    var rename = require("gulp-rename");
-    var print = require('gulp-print').default;
+	var gulp = require("gulp");
+	var cfg = require("./cfg.json");
+	var pkg = require('../package.json');
+	var babel = require('gulp-babel');
+	var uglify = require("gulp-uglify");
+	var rename = require("gulp-rename");
+	var print = require('gulp-print').default;
 
-    gulp
-        .src([
-            "includes/builders/assets/*.js",
-            "!includes/**/*.min.js"
-        ])
-        .pipe(uglify())
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest("includes/builders/assets"))
-        .pipe(print())
-    ;
+	if (1)
+		gulp
+			.src([
+				"includes/builders/assets/*.js",
+				"!includes/**/*.min.js"
+			])
+			.pipe(uglify())
+			.pipe(rename({suffix: ".min"}))
+			.pipe(gulp.dest("includes/builders/assets"))
+			.pipe(print())
+		;
 
-    gulp
-        .src([
-            "includes/builders/gutenberg/assets/js/*.js",
-            "!includes/**/*.min.js"
-        ])
-        .pipe(uglify())
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest("includes/builders/gutenberg/assets/js"))
-        .pipe(print())
-    ;
+	gulp
+		.src([
+			"includes/builders/gutenberg/assets/js/*.js",
+			"!includes/**/*.min.js"
+		])
+		.pipe(babel({presets: ['@babel/env']}))
+		.pipe(uglify())
+		.pipe(rename({suffix: ".min"}))
+		.pipe(gulp.dest("includes/builders/gutenberg/assets/js"))
+		.pipe(print())
+	;
+	if (1)
+		gulp
+			.src([
+				"includes/builders/gutenberg/assets/js/dist/*.js",
+				"!includes/**/*.min.js"
+			])
+			.pipe(uglify())
+			.pipe(rename({suffix: ".min"}))
+			.pipe(gulp.dest("includes/builders/gutenberg/assets/js/dist"))
+			.pipe(print())
+		;
 
-    gulp
-        .src([
-            "includes/builders/gutenberg/assets/js/dist/*.js",
-            "!includes/**/*.min.js"
-        ])
-        .pipe(uglify())
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest("includes/builders/gutenberg/assets/js/dist"))
-        .pipe(print())
-    ;
+	if (1)
+		gulp
+			.src([cfg.path.js + "/**/*.js", "!" + cfg.path.js + "/**/*.min.js"])
+			.pipe(uglify())
+			.pipe(rename({suffix: ".min"}))
+			.pipe(gulp.dest(cfg.path.js))
+			.pipe(print())
+		;
 
-    return gulp
-        .src([cfg.path.js + "/**/*.js", "!" + cfg.path.js + "/**/*.min.js"])
-        .pipe(uglify())
-        .pipe(rename({suffix: ".min"}))
-        .pipe(gulp.dest(cfg.path.js))
-        .pipe(print())
-        ;
+	cb();
 };
 
 
