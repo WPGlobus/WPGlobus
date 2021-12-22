00dccb5a81ea5a9b417974332e8725f484440057
alexgff <alex.wpstarter@gmail.com>
2021-12-22 18:03:40 +0700

    (Builders/RankMathSEO) Added support multilingual columns on `edit.php` page.

--- includes/builders/rank_math_seo/class-wpglobus-rank_math_seo-functions.php
+++ includes/builders/rank_math_seo/class-wpglobus-rank_math_seo-functions.php
@@ -3,6 +3,7 @@
  * File: class-wpglobus-rank_math_seo-functions.php
  *
  * @since 2.4.3
+ * @since 2.8.9 Added support multilingual columns on `edit.php` page.
  *
  * @package WPGlobus\Builders\RankMathSEO.
  * @author  Alex Gor(alexgff)
@@ -34,30 +35,85 @@ if ( ! class_exists( 'WPGlobus_RankMathSEO_Functions' ) ) :
 		 * Constructor.
 		 */
 		public static function controller() {
-
-			if ( empty( $_POST['nonce_CMB2phprank_math_metabox'] ) || empty( $_POST['action'] ) ) {
-				/**
-				 * Not `Rank Math SEO`.
-				 */
-				return;
-			}
-
-			global $pagenow;
-
-			if ( ! empty( $_POST[  WPGlobus::get_language_meta_key() ] ) ) {
-				self::$current_language = sanitize_text_field( $_POST[  WPGlobus::get_language_meta_key() ] );
-			}
 			
-			if ( 'edit-tags.php' === $pagenow && 'editedtag' === $_POST['action'] ) { // phpcs:ignore WordPress.CSRF.NonceVerification
+			if ( WPGlobus_WP::is_pagenow( 'edit.php' ) ) {
+				
 				/**
-				 * Update button was clicked.
+				 * To translate SEO columns on `edit.php` page.
+				 *
+				 * @since 2.8.9
 				 */
-				self::build_ml_description();
-			}
+				add_filter( 'get_post_metadata', array( __CLASS__, 'filter__post_metadata' ), 2, 4 );
+			}				
 
-			add_filter( 'wp_update_term_data', array( __CLASS__, 'filter__update_term_data' ), 5, 4 );
+			if ( is_admin() ) {
+
+				if ( empty( $_POST['nonce_CMB2phprank_math_metabox'] ) || empty( $_POST['action'] ) ) {
+					/**
+					 * Not `Rank Math SEO`.
+					 */
+					return;
+				}
+
+				global $pagenow;
+
+				if ( ! empty( $_POST[  WPGlobus::get_language_meta_key() ] ) ) {
+					self::$current_language = sanitize_text_field( $_POST[  WPGlobus::get_language_meta_key() ] );
+				}
+				
+				if ( 'edit-tags.php' === $pagenow && 'editedtag' === $_POST['action'] ) { // phpcs:ignore WordPress.CSRF.NonceVerification
+					/**
+					 * Update button was clicked.
+					 */
+					self::build_ml_description();
+				}
+
+				add_filter( 'wp_update_term_data', array( __CLASS__, 'filter__update_term_data' ), 5, 4 );
+			}
 		}
 
+		/**
+		 * @since 2.8.9
+		 */
+		public static function filter__post_metadata( $check, $object_id, $meta_key, $single ) {
+
+			/**
+			 * @todo Get keys from builder.
+			 * @since 2.8.9
+			 */
+			$enabled_keys = array(
+				'rank_math_title',
+				'rank_math_description',
+				'rank_math_focus_keyword',
+				'rank_math_seo_score'
+			);
+
+			if ( ! in_array( $meta_key, $enabled_keys ) ) {
+				return $check;
+			}
+
+			$meta_type = 'post';
+
+			$meta_cache = wp_cache_get( $object_id, $meta_type . '_meta' );
+
+			if ( ! $meta_cache ) {
+				return $check;
+			}
+			
+			if ( ! empty( $meta_cache[$meta_key][0] ) && WPGlobus_Core::has_translations( $meta_cache[$meta_key][0] ) ) {
+				
+				$meta_cache[$meta_key][0] = WPGlobus_Core::text_filter( $meta_cache[$meta_key][0], WPGlobus::Config()->language );
+				
+				wp_cache_replace(
+					$object_id,
+					$meta_cache,
+					$meta_type . '_meta'
+				);
+			}
+
+			return $check;
+		}
+		
 		/**
 		 * Build multilingual description.
 		 * We don't have filter for description like filter for name @see 'wp_update_term_data' in wp-includes\taxonomy.php
