2b3aa89030b4e0ae3c9400fa1a42d932e63e3d72
Gregory Karpinsky <tiv.net.inc@gmail.com>
2023-06-04 11:04:09 -0400

    phpcs.cmd - fix for IDE

--- phpcs.cmd
+++ phpcs.cmd
@@ -1,2 +1,3 @@
 @echo off
-%~dp0vendor\bin\phpcs.bat --standard=./phpcs.xml --warning-severity=1 --ignore-annotations --report-full --extensions=php,html,inc --no-colors --ignore=*Test.php %*
+%~dp0vendor\bin\phpcs.bat --warning-severity=1 --ignore-annotations %*
+: %~dp0vendor\bin\phpcs.bat --standard=./phpcs.xml --warning-severity=1 --ignore-annotations --report-full --extensions=php,html,inc --no-colors --ignore=*Test.php %*
